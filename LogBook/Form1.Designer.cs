681c4193fe9a306ca20f83dece5dfb2304d5238a
camalzade_elvin <camalzade_elvin@outlook.com>
2019-01-21 21:47:34 +0400

    Add project files.

---
+++ LogBook/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace LogBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroRadioButton1 = new MetroFramework.Controls.MetroRadioButton();
+            this.metroRadioButton2 = new MetroFramework.Controls.MetroRadioButton();
+            this.metroRadioButton3 = new MetroFramework.Controls.MetroRadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // metroRadioButton1
+            // 
+            this.metroRadioButton1.Location = new System.Drawing.Point(303, 136);
+            this.metroRadioButton1.Name = "metroRadioButton1";
+            this.metroRadioButton1.Size = new System.Drawing.Size(13, 17);
+            this.metroRadioButton1.TabIndex = 0;
+            this.metroRadioButton1.TabStop = true;
+            this.metroRadioButton1.Text = "metroRadioButton1";
+            this.metroRadioButton1.UseVisualStyleBackColor = true;
+            // 
+            // metroRadioButton2
+            // 
+            this.metroRadioButton2.Location = new System.Drawing.Point(399, 136);
+            this.metroRadioButton2.Name = "metroRadioButton2";
+            this.metroRadioButton2.Size = new System.Drawing.Size(13, 17);
+            this.metroRadioButton2.TabIndex = 0;
+            this.metroRadioButton2.TabStop = true;
+            this.metroRadioButton2.Text = "metroRadioButton1";
+            this.metroRadioButton2.UseVisualStyleBackColor = true;
+            // 
+            // metroRadioButton3
+            // 
+            this.metroRadioButton3.Location = new System.Drawing.Point(349, 136);
+            this.metroRadioButton3.Name = "metroRadioButton3";
+            this.metroRadioButton3.Size = new System.Drawing.Size(13, 17);
+            this.metroRadioButton3.TabIndex = 0;
+            this.metroRadioButton3.TabStop = true;
+            this.metroRadioButton3.Text = "metroRadioButton1";
+            this.metroRadioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(1, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(999, 55);
+            this.label1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1001, 621);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.metroRadioButton3);
+            this.Controls.Add(this.metroRadioButton2);
+            this.Controls.Add(this.metroRadioButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private MetroFramework.Controls.MetroRadioButton metroRadioButton1;
+        private MetroFramework.Controls.MetroRadioButton metroRadioButton2;
+        private MetroFramework.Controls.MetroRadioButton metroRadioButton3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
