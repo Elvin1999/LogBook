71418f46c7b98101eb2671217c3eee5cabdf57a2
camalzade_elvin <camalzade_elvin@outlook.com>
2019-01-22 21:41:48 +0400

    Upside

--- LogBook/Form1.Designer.cs
+++ LogBook/Form1.Designer.cs
@@ -32,11 +32,18 @@
             this.metroRadioButton2 = new MetroFramework.Controls.MetroRadioButton();
             this.metroRadioButton3 = new MetroFramework.Controls.MetroRadioButton();
             this.label1 = new System.Windows.Forms.Label();
+            this.labelNameSur = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // metroRadioButton1
             // 
-            this.metroRadioButton1.Location = new System.Drawing.Point(303, 136);
+            this.metroRadioButton1.Location = new System.Drawing.Point(500, 267);
             this.metroRadioButton1.Name = "metroRadioButton1";
             this.metroRadioButton1.Size = new System.Drawing.Size(13, 17);
             this.metroRadioButton1.TabIndex = 0;
@@ -46,7 +53,7 @@
             // 
             // metroRadioButton2
             // 
-            this.metroRadioButton2.Location = new System.Drawing.Point(399, 136);
+            this.metroRadioButton2.Location = new System.Drawing.Point(596, 267);
             this.metroRadioButton2.Name = "metroRadioButton2";
             this.metroRadioButton2.Size = new System.Drawing.Size(13, 17);
             this.metroRadioButton2.TabIndex = 0;
@@ -56,7 +63,7 @@
             // 
             // metroRadioButton3
             // 
-            this.metroRadioButton3.Location = new System.Drawing.Point(349, 136);
+            this.metroRadioButton3.Location = new System.Drawing.Point(546, 267);
             this.metroRadioButton3.Name = "metroRadioButton3";
             this.metroRadioButton3.Size = new System.Drawing.Size(13, 17);
             this.metroRadioButton3.TabIndex = 0;
@@ -66,17 +73,95 @@
             // 
             // label1
             // 
-            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.label1.Location = new System.Drawing.Point(1, 78);
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(246)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(12, 78);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(999, 55);
+            this.label1.Size = new System.Drawing.Size(1348, 55);
             this.label1.TabIndex = 1;
+            // 
+            // labelNameSur
+            // 
+            this.labelNameSur.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(246)))), ((int)(((byte)(255)))));
+            this.labelNameSur.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelNameSur.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(168)))), ((int)(((byte)(243)))));
+            this.labelNameSur.Location = new System.Drawing.Point(128, 95);
+            this.labelNameSur.Name = "labelNameSur";
+            this.labelNameSur.Size = new System.Drawing.Size(150, 23);
+            this.labelNameSur.TabIndex = 2;
+            this.labelNameSur.Text = "Telebenin Ad,Soyadi";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(246)))), ((int)(((byte)(255)))));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(168)))), ((int)(((byte)(243)))));
+            this.label2.Location = new System.Drawing.Point(345, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mystat a daxil olub";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(246)))), ((int)(((byte)(255)))));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(168)))), ((int)(((byte)(243)))));
+            this.label3.Location = new System.Drawing.Point(593, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Her kesi qeyd etmek";
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(246)))), ((int)(((byte)(255)))));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(168)))), ((int)(((byte)(243)))));
+            this.label6.Location = new System.Drawing.Point(1163, 95);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 23);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "5";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(246)))), ((int)(((byte)(255)))));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(168)))), ((int)(((byte)(243)))));
+            this.label4.Location = new System.Drawing.Point(1058, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 23);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Sinifde iş";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(246)))), ((int)(((byte)(255)))));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(168)))), ((int)(((byte)(243)))));
+            this.label5.Location = new System.Drawing.Point(944, 95);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 23);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Yoxlama işlər";
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(246)))), ((int)(((byte)(255)))));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(168)))), ((int)(((byte)(243)))));
+            this.label7.Location = new System.Drawing.Point(1291, 95);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 23);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Rəy";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1001, 621);
+            this.ClientSize = new System.Drawing.Size(1351, 621);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelNameSur);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.metroRadioButton3);
             this.Controls.Add(this.metroRadioButton2);
@@ -92,6 +177,13 @@
         private MetroFramework.Controls.MetroRadioButton metroRadioButton2;
         private MetroFramework.Controls.MetroRadioButton metroRadioButton3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelNameSur;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
     }
 }
 
