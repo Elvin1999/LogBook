97c57f3d1261a6a2ae1fb8d9379406c36d50ab21
camalzade_elvin <camalzade_elvin@outlook.com>
2019-01-25 21:03:07 +0400

    User class is ready . . .

---
+++ LogBook/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBook
+{
+    class User
+    {
+        public string FullName { get; set; }
+        public DateTime AccessDate { get; set; }
+        public bool IsTakePartIn { get; set; }
+        public bool IsLate { get; set; }
+        public bool IsAbsent { get; set; }
+        public int CheckPoint { get; set; }
+        public int ClassPoint { get; set; }
+        public int DiamondCount { get; set; }
+    }
+}
